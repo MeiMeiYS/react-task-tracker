3dbb3bb5fc5728e00de27e9fc674d6a5bc203657
Mei <86375959+MeiMeiYS@users.noreply.github.com>
2021-08-16 14:11:52 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAddTask }) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color={showAddTask ? 'red' : 'green'} text={showAddTask ? 'Close' : 'Add'} onClick={onAdd} />
+        </header>
+    )
+}
+
+Header.defaultProps ={
+    title: 'Task Tracker'
+}
+
+Header.prototype = {
+    title: PropTypes.string.isRequired
+}
+
+export default Header
